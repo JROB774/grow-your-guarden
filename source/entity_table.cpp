cfa201c8b6482e563105301aaff0ea5029293135
Joshua Robertson <joshrobertson774@gmail.com>
2023-02-02 19:27:43 +0000

    Expand the entity table to be easier to view and edit.

--- source/entity_table.cpp
+++ source/entity_table.cpp
@@ -6,6 +6,7 @@ DEF_ETICK(walker);
 
 enum: nkU32
 {
+
 EntityID_None,
 EntityID_Daisy,
 EntityID_Bramble,
@@ -13,18 +14,109 @@ EntityID_Walker,
 EntityID_Pollen,
 EntityID_House,
 EntityID_TOTAL
+
 };
 
 INTERNAL constexpr EntityDesc ENTITY_TABLE[] =
 {
-// Texture              Animation Group      Start Anim     Behavior        Type                Health    Damage    Speed    Range   Collision           Bounds         Phases
-{  NULL,                NULL,                NULL,          NULL,           EntityType_None,      0.0f,     0.0f,    0.0f,    0.0f,  EntityType_None,    { 0.0f,0.0f }, {  0, 0, 0, 0, 0, 0, 0, 0 } }, // EntityID_None
-{  "ent_p/daisy.png",   "ent_p/daisy.anm",   "phase0_idle", ETICK(daisy),   EntityType_Plant,     3.0f,     0.0f,    0.0f,    4.5f,  EntityType_None,    { 1.0f,1.0f }, { 15,20, 0, 0, 0, 0, 0, 0 } }, // EntityID_Daisy
-{  "ent_p/bramble.png", "ent_p/bramble.anm", "phase0_idle", ETICK(bramble), EntityType_Plant,     3.0f,     0.0f,    0.0f,    0.0f,  EntityType_None,    { 1.0f,1.0f }, { 30,40,50, 0, 0, 0, 0, 0 } }, // EntityID_Bramble
-{  "ent_m/walker.png",  "ent_m/walker.anm",  "walk",        ETICK(walker),  EntityType_Monster,  10.0f,     1.0f,    0.5f,    0.0f,  EntityType_None,    { 1.0f,1.0f }, {  0, 0, 0, 0, 0, 0, 0, 0 } }, // EntityID_Walker
-{  "ent_b/pollen.png",  "ent_b/pollen.anm",  "idle",        NULL,           EntityType_Bullet,    0.0f,     2.0f,    3.1f,    5.0f,  EntityType_Monster, { 0.5f,0.5f }, {  0, 0, 0, 0, 0, 0, 0, 0 } }, // EntityID_Pollen
-{  "ent_o/house.png",   "ent_o/house.anm",   "idle",        NULL,           EntityType_Base,    100.0f,     0.0f,    0.0f,    0.0f,  EntityType_None,    { 3.0f,3.0f }, {  0, 0, 0, 0, 0, 0, 0, 0 } }, // EntityID_House
-};
+
+// EntityID_None
+{
+/* Texture         */ NULL,
+/* Animation Group */ NULL,
+/* Start Animation */ NULL,
+/* Tick            */ NULL,
+/* Type            */ EntityType_None,
+/* Health          */ 0.0f,
+/* Damage          */ 0.0f,
+/* Speed           */ 0.0f,
+/* Range           */ 0.0f,
+/* Collision Mask  */ EntityType_None,
+/* Bounds          */ { 0.0f,0.0f },
+/* Growth Phases   */ { 0,0,0,0,0,0,0,0 }
+},
+
+// EntityID_Daisy
+{
+/* Texture         */ "ent_p/daisy.png",
+/* Animation Group */ "ent_p/daisy.anm",
+/* Start Animation */ "phase0_idle",
+/* Tick            */ ETICK(daisy),
+/* Type            */ EntityType_Plant,
+/* Health          */ 5.0f,
+/* Damage          */ 0.0f,
+/* Speed           */ 0.0f,
+/* Range           */ 4.5f,
+/* Collision Mask  */ EntityType_None,
+/* Bounds          */ { 1.0f,1.0f },
+/* Growth Phases   */ { 2,2,0,0,0,0,0,0 }
+},
+
+// EntityID_Bramble
+{
+/* Texture         */ "ent_p/bramble.png",
+/* Animation Group */ "ent_p/bramble.anm",
+/* Start Animation */ "phase0_idle",
+/* Tick            */ ETICK(bramble),
+/* Type            */ EntityType_Plant,
+/* Health          */ 5.0f,
+/* Damage          */ 0.0f,
+/* Speed           */ 0.0f,
+/* Range           */ 0.0f,
+/* Collision Mask  */ EntityType_None,
+/* Bounds          */ { 1.0f,1.0f },
+/* Growth Phases   */ { 30,40,50,0,0,0,0,0 }
+},
+
+// EntityID_Walker
+{
+/* Texture         */ "ent_m/walker.png",
+/* Animation Group */ "ent_m/walker.anm",
+/* Start Animation */ "walk",
+/* Tick            */ ETICK(walker),
+/* Type            */ EntityType_Monster,
+/* Health          */ 10.0f,
+/* Damage          */ 1.0f,
+/* Speed           */ 0.5f,
+/* Range           */ 0.0f,
+/* Collision Mask  */ EntityType_None,
+/* Bounds          */ { 1.0f,1.0f },
+/* Growth Phases   */ { 0,0,0,0,0,0,0,0 }
+},
+
+// EntityID_Pollen
+{
+/* Texture         */ "ent_b/pollen.png",
+/* Animation Group */ "ent_b/pollen.anm",
+/* Start Animation */ "idle",
+/* Tick            */ NULL,
+/* Type            */ EntityType_Bullet,
+/* Health          */ 0.0f,
+/* Damage          */ 2.0f,
+/* Speed           */ 3.1f,
+/* Range           */ 5.0f,
+/* Collision Mask  */ EntityType_Monster,
+/* Bounds          */ { 0.5f,0.5f },
+/* Growth Phases   */ { 0,0,0,0,0,0,0,0 }
+},
+
+// EntityID_House
+{
+/* Texture         */ "ent_o/house.png",
+/* Animation Group */ "ent_o/house.anm",
+/* Start Animation */ "idle",
+/* Tick            */ NULL,
+/* Type            */ EntityType_Base,
+/* Health          */ 100.0f,
+/* Damage          */ 0.0f,
+/* Speed           */ 0.0f,
+/* Range           */ 0.0f,
+/* Collision Mask  */ EntityType_None,
+/* Bounds          */ { 3.0f,3.0f },
+/* Growth Phases   */ { 0,0,0,0,0,0,0,0 }
+},
+
+}; // ENTITY_TABLE
 
 NK_STATIC_ASSERT(NK_ARRAY_SIZE(ENTITY_TABLE) == EntityID_TOTAL, entity_table_size_mismatch);
 
